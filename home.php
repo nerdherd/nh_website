149a551918976dbc6b1905314f46cb41449fd000
Misbah Khan <mk.misbah.khan@gmail.com>
2011-07-28 23:55:25 -0700

    Added a function for more dynamic linking of home tiles to their respective pages.

--- home.php
+++ home.php
@@ -4,13 +4,21 @@ Template Name: Home
 */
 ?>
 <?php get_header(); ?>
+<?php 
+function url($title){
+$url = get_site_url();
+$page = get_page_by_title($title);
+$pgid = $page->ID;
+echo '<a href="'.$url.'/?page_id='.$pgid.'"><span class="text">'.$title.'</span></a>';
+}
+?>
 <div class="content">
 <div class="toast"><div id="ttext">VEX Summer camps you should send your kids<div class="x">x</div></div></div>
-<div id="logs"><div class="s_tile"></div></div>
-<div id="thenerds"><div class="s_tile"></div></div>
-<div id="outreach"><div class="s_tile"><a href="/programs.html"><span class="text">Outreach</span></a></div></div>
-<div id="theherd"><div class="l_tile"><a href="/test/theherd.html"><span class="text">The Herd</span></a></div></div>
-<div id="sponsors"><div class="s_tile"><a href="/programs.html"><span class="text">Sponsors</span></a></div></div>
+<div id="logs"><div class="s_tile"><?=url('Blogs')?></div></div>
+<div id="thenerds"><div class="s_tile"><?=url('The Nerds')?></div></div>
+<div id="outreach"><div class="s_tile"><?=url('Outreach')?></div></div>
+<div id="theherd"><div class="l_tile"><?=url('The Herd')?></div></div>
+<div id="sponsors"><div class="s_tile"><?=url('Sponsors')?></div></div>
 <div id="lorem"></div>
 <br/>
 <br/>
