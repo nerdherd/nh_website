a94114452806a51dd3631a12ee235e28e98f58b0
Misbah Khan <mk.misbah.khan@gmail.com>
2011-07-28 23:16:24 -0700

    fixed nav on pages with posts

--- home.php
+++ home.php
@@ -4,15 +4,16 @@ Template Name: Home
 */
 ?>
 <?php get_header(); ?>
-<div id="home">
 <div class="content">
-
-<div id="logs"><div class="s_tile"><a href="/programs.html"><span class="text">Blogs</span></a></div></div>
-<div id="thenerds"><div class="s_tile"><a href="/programs.html"><span class="text">The Nerds</span></a></div></div>
+<div class="toast"><div id="ttext">VEX Summer camps you should send your kids<div class="x">x</div></div></div>
+<div id="logs"><div class="s_tile"></div></div>
+<div id="thenerds"><div class="s_tile"></div></div>
 <div id="outreach"><div class="s_tile"><a href="/programs.html"><span class="text">Outreach</span></a></div></div>
-<div id="theherd"><div class="l_tile"><a href="/programs.html"><span class="text">The Herd</span></a></div></div>
+<div id="theherd"><div class="l_tile"><a href="/test/theherd.html"><span class="text">The Herd</span></a></div></div>
 <div id="sponsors"><div class="s_tile"><a href="/programs.html"><span class="text">Sponsors</span></a></div></div>
-</div>
-</div>
+<div id="lorem"></div>
+<br/>
+<br/>
+<br/>
 </div>
 <?php get_footer(); ?>
